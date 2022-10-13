62d8aa94e1f3be5ca85f88549dd9e1b3d76f822a
Thomasdjb <darkchameau@gmail.com>
2022-10-13 13:51:15 +0200

    added plot callback

--- wonders_classifier.py
+++ wonders_classifier.py
@@ -114,6 +114,47 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath="C:/Users/duboisrouvra
                                                  initial_value_threshold = init_threshold,
                                                  verbose=1)
 
+class PlotLearning(tf.keras.callbacks.Callback):
+    """
+    Callback to plot the learning curves of the model during training.
+    """
+    def on_train_begin(self, logs={}):
+        self.metrics = {}
+        for metric in logs:
+            self.metrics[metric] = []
+            
+
+    def on_epoch_end(self, epoch, logs={}):
+        # Storing metrics
+        for metric in logs:
+            if metric in self.metrics:
+                self.metrics[metric].append(logs.get(metric))
+            else:
+                self.metrics[metric] = [logs.get(metric)]
+        
+        # Plotting
+        metrics = [x for x in logs if 'val' not in x]
+        
+        f, axs = plt.subplots(1, len(metrics), figsize=(15,5))
+        clear_output(wait=True)
+
+        for i, metric in enumerate(metrics):
+            axs[i].plot(range(1, epoch + 2), 
+                        self.metrics[metric], 
+                        label=metric)
+            if logs['val_' + metric]:
+                axs[i].plot(range(1, epoch + 2), 
+                            self.metrics['val_' + metric], 
+                            label='val_' + metric)
+                
+            axs[i].legend()
+            axs[i].grid()
+
+        plt.tight_layout()
+        plt.show()
+
+plt_callback = PlotLearning()
+
 #################################################################
 ########################## TRAINING #############################
 #################################################################
